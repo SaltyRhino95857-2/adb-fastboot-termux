0a229c32b81d93c0896c801cef90a09e86a77a25
SaltyRhino95857-2 <128185610+SaltyRhino95857-2@users.noreply.github.com>
2023-12-26 15:44:54 +0200

    Import only system() instead of whole 'os'

--- af.py
+++ af.py
@@ -1,6 +1,6 @@
-import os
+from os import system
 
-os.system("clear")
+system("clear")
 
 print("Android ADB and FASTBOOT Installer")
 print("script by freetheorange905 0.1")
@@ -10,9 +10,9 @@ print("[2]Uninstall ADB and FASTBOOT")
 print("")
 choice = input("Choice:")
 if choice == "1":
-	os.system("apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -q && bash InstallTools.sh")
+	system("apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -q && bash InstallTools.sh")
 elif choice == "2":
-	os.system("apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB/raw/master/RemoveTools.sh -q && bash RemoveTools.sh")
+	system("apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB/raw/master/RemoveTools.sh -q && bash RemoveTools.sh")
 else:
 	print("Invalid choice.)
 	exit(1)
